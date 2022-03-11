1b55ab4eb5bd1b0a488dd352bfee7d9d023c64d0
Sepehr Pourabolfath Hashtroudi <sepehr8@gra-login3.graham.sharcnet>
2022-03-11 08:12:07 -0500

    dataset

--- dataset/combined/show_examples.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-example = pd.read_csv( "train_combined.csv", header=None)
-print(example.loc[0,0])
-print(example.loc[0,1])
--- dataset/small/make_csv.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-methods = pd.read_csv( "train_final_500.methods", sep="\n",lineterminator="\n", header=None)
-tests = pd.read_csv("train_final_500.tests", sep="\n",lineterminator="\n", header=None)
-df = pd.concat([methods, tests], axis=1)
-df.to_csv("train_final_500.csv", encoding='utf-8', index=False)
-methods = pd.read_csv( "eval_final_500.methods", sep="\n",lineterminator="\n", header=None)
-tests = pd.read_csv("eval_final_500.tests", sep="\n",lineterminator="\n", header=None)
-df = pd.concat([methods, tests], axis=1)
-df.to_csv("eval_final_500.csv", encoding='utf-8', index=False)
