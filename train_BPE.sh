ceb6603d2ee1e1c81ea15eb1c40f3cdc9b80376b
Sepehr Pourabolfath Hashtroudi <sepehr8@gra-login3.graham.sharcnet>
2022-03-11 09:17:28 -0500

    working stable

--- train_BPE.sh
+++ train_BPE.sh
@@ -2,7 +2,7 @@
 #SBATCH --gres=gpu:4       # Request GPU "generic resources"
 #SBATCH --cpus-per-task=8  # Refer to cluster's documentation for the right CPU/GPU ratio
 #SBATCH --mem=32000M       # Memory proportional to GPUs: 32000 Cedar, 47000 Béluga, 64000 Graham.
-#SBATCH --time=2-8:00     # DD-HH:MM:SS
+#SBATCH --time=3-12:00     # DD-HH:MM:SS
 
 source ENV/bin/activate
 python BART_BPE.py
